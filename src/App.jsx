b68109d4083323984eabb5c609d7db18a001724f
mayedakonika <mayedakonika@gmail.com>
2024-01-13 01:58:42 +0600

    after clicking the X print added to UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,43 @@
-const Square = () => {
+/* eslint-disable react/prop-types */
+
+import { useState } from "react";
+
+const Square = ({ value, onSquareClick }) => {
+  console.log(value);
   return (
-    <button className="bg-white border border-gray-800 h-12 w-12 m-1 text-3xl">
-      x
+    <button
+      onClick={onSquareClick}
+      className="bg-white border border-gray-800 h-12 w-12 m-1 text-3xl"
+    >
+      {value}
     </button>
   );
 };
 
 const Board = () => {
+  const [square, setSquare] = useState(Array(9).fill(null));
+  const handleClick = (value) => {
+    const newSqares = [...square];
+    newSqares[value] = "X";
+    console.log(newSqares);
+    setSquare(newSqares);
+  };
   return (
     <>
-      <div>
-        <Square />
-        <Square />
-        <Square />
+      <div className="flex">
+        <Square value={square[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={square[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={square[2]} onSquareClick={() => handleClick(2)} />
       </div>
-      <div>
-        <Square />
-        <Square />
-        <Square />
+      <div className="flex">
+        <Square value={square[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={square[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={square[5]} onSquareClick={() => handleClick(5)} />
       </div>
-      <div>
-        <Square />
-        <Square />
-        <Square />
+      <div className="flex">
+        <Square value={square[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={square[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={square[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
   );
