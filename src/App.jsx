14c9ea52f3d4cb1855aa26668b6b89bf524e1521
mayedakonika <mayedakonika@gmail.com>
2024-01-13 04:28:00 +0600

    X and O winner search implementation

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,15 @@ const Square = ({ value, onSquareClick }) => {
 const Board = () => {
   const [square, setSquare] = useState(Array(9).fill(null));
   const [isXNext, setIsXNext] = useState();
+
+  const winner = calculateWinner(square);
+  let gameStatus;
+  if (winner) {
+    gameStatus = `Winner ${winner}`;
+  } else {
+    gameStatus = `Next Player ${winner}`;
+  }
+
   const handleClick = (i) => {
     const newSquare = [...square];
     if (square[i]) {
@@ -29,8 +38,10 @@ const Board = () => {
     setIsXNext(!isXNext);
     setSquare(newSquare);
   };
+
   return (
     <>
+      <h1>{gameStatus}</h1>
       <div className="flex">
         <Square value={square[0]} onSquareClick={() => handleClick(0)} />
         <Square value={square[1]} onSquareClick={() => handleClick(1)} />
@@ -51,3 +62,24 @@ const Board = () => {
 };
 
 export default Board;
+
+const calculateWinner = (square) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (square[a] && square[a] === square[b] && square[a] === square[c]) {
+      return [a];
+    }
+  }
+  return null;
+};
